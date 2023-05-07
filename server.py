879d54cf0a2dbd18155d999cbd14b75cd1766685
tkoksal <tamerkoksal@hotmail.com>
2023-05-07 15:54:54 +0100

    modified some display features

--- server.py
+++ server.py
@@ -36,8 +36,11 @@ def write_to_csv(data):
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
-        data = request.form.to_dict()
-        write_to_csv(data)
-        return redirect('/thankyou.html')
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'did not save to database.'
     else:
         return 'something went wrong. Try again!'
